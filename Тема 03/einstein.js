22eb92cbbe19ea5ecf5f7524527268c5217a216f
Valeriya <leradenisenko.9@gmail.com>
2023-10-05 11:46:51 +0600

    Тема 3

---
+++ Тема 03/einstein.js
@@ -0,0 +1,8 @@
+'use strict'
+
+{
+const myWeight = 50;
+const speedLight = 3e8;
+
+console.log("Энергия тела = " + myWeight*Math.pow(speedLight, 2));
+}
--- Тема 03/index.js
+++ Тема 03/index.js
@@ -7,15 +7,38 @@
   const productPrice = 25;
 
   console.log(productName);
-  console.log('Общая сумма товара ' productAmount*productPrice);
+  console.log('Общая сумма товара ', productAmount*productPrice);
 }
 
 {
     const productName = 'Булочка с корицей';
     const productAmount = 10;
-    const productСategory = 'хлебобулочные изделия';
+    const productСategory = 'Хлебобулочные изделия';
     const productPrice = 45;
   
     console.log(productName);
-    console.log('Общая сумма товара ' productAmount*productPrice);
+    console.log('Общая сумма товара ', productAmount*productPrice);
 }
+
+{
+  const productName = 'Свеча';
+  const productAmount = 25;
+  const productСategory = 'Декор';
+  const productPrice = 93;
+
+  console.log(productName);
+  console.log('Общая сумма товара ', productAmount*productPrice);
+}
+
+const productName = prompt('Введите наименование товара');
+const productAmount = Number(prompt('Введите количество товара'));
+const productСategory = prompt('Введите категорию товара');
+const productPrice = Number(prompt('Введите цену товара'));
+
+console.log('Тип данных: ' + typeof productName);
+console.log('Тип данных: ' + typeof productAmount);
+console.log('Тип данных: ' + typeof productСategory);
+console.log('Тип данных: ' + typeof productPrice);
+
+console.log('На складе ' + productAmount + ' единиц товара ' + productName + ' на сумму ' + productAmount*productPrice + ' деревянных');
+
